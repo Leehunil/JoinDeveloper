36c9ae44d134e685cfb3917da145614ed810101e
Leehunil <hunil9978@gmail.com>
2023-01-19 19:43:21 +0900

    개발 환경 구축 및 Maria DB 연결

---
+++ src/main/java/GDSC/JoinDeveloper/JoinDeveloperApplication.java
@@ -0,0 +1,13 @@
+package GDSC.JoinDeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinDeveloperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GDSC/JoinDeveloper/JoinDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package GDSC.JoinDeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
